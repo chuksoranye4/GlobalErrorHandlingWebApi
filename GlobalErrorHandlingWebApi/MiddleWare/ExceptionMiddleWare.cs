bbddb7ee2d08a8acfcced19967e36d438d8adb81
chuksoranye4 <chuksoranye4@yahoo.com>
2023-03-21 16:12:47 +0100

    Add project files.

---
+++ GlobalErrorHandlingWebApi/MiddleWare/ExceptionMiddleWare.cs
@@ -0,0 +1,32 @@
+﻿namespace GlobalErrorHandlingWebApi.MiddleWare;
+
+public class ExceptionMiddleWare
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionMiddleWare(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(httpContext, ex);
+        }
+    }
+    private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        await context.Response.WriteAsync(new ErrorDetails()
+        {
+            StatusCode = context.Response.StatusCode,
+            Message = "Internal Server Error from the custom middleware."
+        }.ToString());
+    }
+}
